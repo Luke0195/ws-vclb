96ae1d81675125474c8786e38feee215483e8e88
Lucas Santos <lucas201273@gmail.com>
2023-12-27 11:39:09 -0300

    feat: add seeding on database

--- src/main/java/br/com/dscatalog/application/services/implementations/CategoryUseCaseImplementation.java
+++ src/main/java/br/com/dscatalog/application/services/implementations/CategoryUseCaseImplementation.java
@@ -3,9 +3,10 @@ package br.com.dscatalog.application.services.implementations;
 import br.com.dscatalog.application.entities.Category;
 import br.com.dscatalog.application.repositories.CategoryRepository;
 import br.com.dscatalog.application.services.usecases.CategoryUseCases;
-import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,7 +23,7 @@ public class CategoryUseCaseImplementation implements CategoryUseCases {
     }
 
     @Override
-    @Transactional
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return categoryRepository.findAll();
     }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_categories(name) VALUES('Eletronics');
+INSERT INTO tb_categories(name) VALUES('Computers');
+INSERT INTO tb_categories(name) VALUES('Books');
