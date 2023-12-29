e27545318f82d69b5bb464d0e4acf3d09593ca5f
Lucas Santos <lucas201273@gmail.com>
2023-12-29 10:17:18 -0300

    feat: implements new usecases onf logmessage

--- src/main/java/br/com/dscatalog/application/controllers/LogMessageController.java
+++ src/main/java/br/com/dscatalog/application/controllers/LogMessageController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/logs")
 public class LogMessageController {
-
     private final LogMessageUseCaseImplementation logMessageUseCaseImplementation;
-
     @Autowired
     public LogMessageController(LogMessageUseCaseImplementation logMessageUseCaseImplementation){
         this.logMessageUseCaseImplementation = logMessageUseCaseImplementation;
@@ -25,5 +24,10 @@ public class LogMessageController {
     @GetMapping
     public ResponseEntity<List<LogMessageDto>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body(logMessageUseCaseImplementation.findAll());
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<LogMessageDto> findById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(logMessageUseCaseImplementation.findErrorMessageById(id));
     }
 }
--- src/main/java/br/com/dscatalog/application/services/implementations/LogMessageUseCaseImplementation.java
+++ src/main/java/br/com/dscatalog/application/services/implementations/LogMessageUseCaseImplementation.java
@@ -4,11 +4,13 @@ import br.com.dscatalog.application.dtos.LogMessageDto;
 import br.com.dscatalog.application.entities.LogMessage;
 import br.com.dscatalog.application.mappers.LogMessageMapper;
 import br.com.dscatalog.application.repositories.LogMessageRepository;
+import br.com.dscatalog.application.services.exceptions.ResourceNotExists;
 import br.com.dscatalog.application.services.usecases.LogMessageUseCase;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LogMessageUseCaseImplementation implements LogMessageUseCase {
@@ -33,6 +35,8 @@ public class LogMessageUseCaseImplementation implements LogMessageUseCase {
 
     @Override
     public LogMessageDto findErrorMessageById(Long id) {
-        return null;
+        Optional<LogMessage> logAlreadyExists = repository.findById(id);
+        LogMessage entity = logAlreadyExists.orElseThrow(() -> new ResourceNotExists("This logmessage doesn't not exists "));
+        return LogMessageMapper.parseEntityToDto(entity);
     }
 }
