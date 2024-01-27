24743ebf8575acd409269cce3f00136552d28fb0
Lucas Santos <lucas201273@gmail.com>
2024-01-27 17:25:10 -0300

    test: ensure product controller returns 404 when an invalid id is provided

--- src/test/java/br/com/dscatalog/application/controllers/ProductControllerTests.java
+++ src/test/java/br/com/dscatalog/application/controllers/ProductControllerTests.java
@@ -2,6 +2,7 @@ package br.com.dscatalog.application.controllers;
 
 import br.com.dscatalog.application.dtos.ProductDto;
 import br.com.dscatalog.application.factories.ProductFactory;
+import br.com.dscatalog.application.services.exceptions.ResourceNotExists;
 import br.com.dscatalog.application.services.implementations.ProductUseCaseImplementation;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -32,17 +33,27 @@ class ProductControllerTests {
 
     private PageImpl<ProductDto> pageDto;
     private ProductDto productDto;
+    private Long nonExistingId;
+
     @BeforeEach
     void setup(){
+        this.nonExistingId = 1000000L;
         this.productDto = new ProductDto(ProductFactory.makeProductWithNoArguments());
         this.pageDto = new PageImpl<>(List.of(this.productDto));
         Mockito.when(productUseCaseImplementation.findAllPaged(ArgumentMatchers.any())).thenReturn(pageDto);
+        Mockito.when(productUseCaseImplementation.findById(this.nonExistingId)).thenThrow(ResourceNotExists.class);
     }
 
     @Test
     void findAllShouldReturnPage() throws Exception{
         ResultActions result = mockMvc.perform(get("/products").accept(MediaType.APPLICATION_JSON));
         result.andExpect(status().isOk());
+    }
+
+    @Test
+    void findByIdShouldReturnAProductWhenIdIsValid() throws Exception{
+      ResultActions result = mockMvc.perform(get("/products/" +this.nonExistingId));
+      result.andExpect(status().isNotFound());
     }
 
 
